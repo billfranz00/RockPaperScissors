175425e2b32b93fd1f4e9e3914fd5295fe81451e
billyf <Maggie91>
2019-11-11 15:46:11 -0800

    Final touches, moving duplicate code into methods, invoking timer for both modes

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const selectButtons = document.getElementsByClassName("js-select-mode"),
 	midContent = document.querySelector(".js-mid-game"),
 	endContent = document.querySelector(".js-end-game"),
 	optionList = document.querySelector(".js-options"),
+	computerType = document.querySelector(".js-computer-type"),
 	userType = document.querySelector(".js-user-type"),
 	userMessage = document.querySelector(".js-user-message"),
 	options = document.getElementsByClassName("js-choose-option"),
@@ -21,92 +22,111 @@ const selectButtons = document.getElementsByClassName("js-select-mode"),
 		3: "scissors"
 	};
 
-let mode;
+let mode,
+	timer;
 
 Array.prototype.forEach.call(selectButtons, button => {
 	button.addEventListener('click', () => {
 		mode = button.value;
-		startGame();
-		userType.innerHTML = mode ? "Player" : "Computer";
-		userMessage.innerHTML = mode ? "Choose Your Move" : "Computer Gets To Choose";
-		console.log(mode);
-		if (!mode) {
-			let time = 5;
-			gameTime.innerHTML = time;
-			const timer = setInterval(() => {
-				time--;
-				gameTime.innerHTML = time;
-				if (time === 0) {
-					finishGame();
-					clearInterval(timer);
-				}
-				console.log(time);
-			}, 1000);
-		}
-		else {
-			Array.prototype.forEach.call(options, option => {
-				option.addEventListener("click", () => {
-					for (let i = 0; i < options.length; i++) {
-						if (option.value !== options[i].value) {
-							options[i].parentElement.className += " game__button--dim";
-						}
-					}
-					finishGame(option.value);
-				});
-			});
-		}
+		computerType.innerHTML = mode ? "Computer" : "Computer 1";
+		userType.innerHTML = mode ? "Player 1" : "Computer 2";
+		userMessage.innerHTML =
+			mode ? "Choose Your Move! You have 5 seconds or else one will be chosen randomly..."
+				: "Computer Gets To Choose";
+		chooseMode();
+		startContent.style.display = "none";
+		midContent.style.display = "";
+		optionList.style.display = "";
 	});
 });
 
 repeatGame.addEventListener('click', () => {
+	chooseMode();
+	removeButtonCovers();
 	midContent.style.display = "";
 	endContent.style.display = "none";
-	for (let i = 0; i < options.length; i++) {
-		options[i].parentElement.classList.remove("game__button--dim");
-	}
 })
 
 finishButton.addEventListener('click', () => {
-	startContent.style.display = "";
 	midContent.style.display = "none";
 	optionList.style.display = "none";
 	endContent.style.display = "none";
-	for (let i = 0; i < options.length; i++) {
-		options[i].parentElement.classList.remove("game__button--dim");
-	}
+	removeButtonCovers();
+	startContent.style.display = "";
 });
-
-function startGame() {
-	startContent.style.display = "none";
-	midContent.style.display = "";
-	optionList.style.display = "";
-}
 
 function finishGame(value) {
 	const playerOne = randomNumberGenerator(),
 		playerTwo = value ? Number(value) : randomNumberGenerator();
 
-	console.log("Player One had " + optionObj[playerOne]);
-	console.log("Player Two had " + optionObj[playerTwo]);
+	for (let i = 0; i < options.length; i++) {
+		if (playerTwo !== Number(options[i].value)) {
+			options[i].parentElement.className += " game__button--dim";
+		}
+	}
 
 	endGameImg.src = `img/${optionObj[playerOne]}.jpg`;
+
+	const targetUser = mode ? "You " : "Computer 2 ";
 
 	if (playerOne === playerTwo) {
 		endGameText.innerHTML = "It's a Tie!!!";
 	} else if ((playerTwo - playerOne + 3) % 3 === 1) {
-		endGameText.innerHTML = "You Win!!!";
+		endGameText.innerHTML = targetUser + "Won!!!";
 	} else {
-		endGameText.innerHTML = "You Lose!!!";
+		endGameText.innerHTML = targetUser + "Lost!!!";
+	}
+
+	if (value) {
+		removeChooseOptionEvent();
 	}
 
 	midContent.style.display = "none";
 	endContent.style.display = "";
 }
 
-function randomNumberGenerator() {
-	return Math.floor(Math.random() * 3) + 1;
+function chooseOption() {
+	clearInterval(timer);
+	finishGame(this.value);
+}
+
+function chooseMode() {
+	invokeTimer();
+	if (!mode) {
+		removeChooseOptionEvent();
+	}
+	else {
+		Array.prototype.forEach.call(options, option => {
+			option.addEventListener("click", chooseOption);
+		});
+	}
 }
 
 function invokeTimer() {
-	
+	let time = 5;
+	gameTime.innerHTML = time;
+	timer = setInterval(() => {
+		time--;
+		gameTime.innerHTML = time;
+		if (time === 0) {
+			finishGame();
+			clearInterval(timer);
+		}
+	}, 1000);
+}
+
+function removeChooseOptionEvent() {
+	Array.prototype.forEach.call(options, button => {
+		button.removeEventListener('click', chooseOption);
+	});
+}
+
+function removeButtonCovers() {
+	for (let i = 0; i < options.length; i++) {
+		options[i].parentElement.classList.remove("game__button--dim");
+	}
+}
+
+function randomNumberGenerator() {
+	return Math.floor(Math.random() * 3) + 1;
 }
